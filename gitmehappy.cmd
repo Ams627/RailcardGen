4d501c199c5200ace43e529fa54b2d09e7f01690
Ams <adriansims62781@gmail.com>
2017-10-08 21:52:30 +0100

    Add project files.

---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add RailcardGen/Program.cs
+git add RailcardGen/App.config
+git add RailcardGen/Properties/AssemblyInfo.cs
+git add RailcardGen/RailcardGen.csproj
+git add RailcardGen.sln
+git add .gitignore
